22dbd75be2f4cf16b05f9d8709572f1428d1da3a
Akash Bhaskar <i.am.akashbhaskar@gmail.com>
2024-10-08 23:55:07 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import Form from "./login/Form";
 export default function LoginPage() {
   const [loggedIn, setLoggedIn] = useState(false);
 
+  console.log(loggedIn, setLoggedIn);
+
   return (
     <div className="container">
       <div className="logo">
